2324b707f6daca6991063aa43444e6d5881a25d5
zhanghan2021 <zhanghan@kylinos.cn>
2024-10-23 19:52:00 +0800

    fix rule table create failed

--- server/model/rule.go
+++ server/model/rule.go
@@ -2,18 +2,14 @@ package model
 
 type Rule struct {
 	ID             int          `gorm:"primary_key;AUTO_INCREMENT" json:"id"`
-	AlertName      string       `gorm:"uniqueIndex:idx_alert" json:"alertName"`                             // 告警名称
-	CustomDesc     string       `gorm:"uniqueIndex:idx_alert" json:"desc"`                                  // 自定义描述
-	MonitorMetrics string       `gorm:"uniqueIndex:idx_alert" json:"metrics"`                               // 监控指标
-	AlarmThreshold string       `gorm:"uniqueIndex:idx_alert" json:"threshold"`                             // 告警阈值
-	Forsearch      string       `gorm:"uniqueIndex:idx_alert" json:"duration"`                              // 触发时长
-	Severity       string       `gorm:"uniqueIndex:idx_alert" json:"severity"`                              // 告警级别
+	AlertName      string       `gorm:"uniqueIndex:idx_alert, length:100" json:"alertName"`                 // 告警名称
+	CustomDesc     string       `gorm:"uniqueIndex:idx_alert, length:100" json:"desc"`                      // 自定义描述
+	MonitorMetrics string       `gorm:"uniqueIndex:idx_alert, length:100" json:"metrics"`                   // 监控指标
+	AlarmThreshold string       `gorm:"uniqueIndex:idx_alert, length:100" json:"threshold"`                 // 告警阈值
+	Forsearch      string       `gorm:"uniqueIndex:idx_alert, length:100" json:"duration"`                  // 触发时长
+	Severity       string       `gorm:"uniqueIndex:idx_alert, length:100" json:"severity"`                  // 告警级别
 	AlertLabel     string       `json:"alertLabel"`                                                         // 告警标签
-	AlertTargets   []RuleTarget `gorm:"foreignKey:RuleId;constraint:OnDelete:CASCADE;" json:"alertTargets"` // 告警机器
-	UserId         int          `json:"userId"`                                                             // 创建规则用户id
-	UserName       string       `json:"userName"`                                                           // 用户名
-	DepartmentId   int          `json:"departmentId"`                                                       // 部门id
-	DepartmentName string       `json:"departmentName"`                                                     // 部门名称
+	AlertTargets   []RuleTarget `gorm:"foreignKey:RuleId;constraint:OnDelete:CASCADE;" json:"alertTargets"` // 告警机器                                                 // 部门名称
 }
 type RuleTarget struct {
 	ID     int    `gorm:"primary_key;AUTO_INCREMENT" json:"id"`
